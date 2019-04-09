9db5ead087efb17f8cbcc6ca305b64212eca97ea
Matyliano <joanna.boszko@gmail.com>
2019-04-09 13:38:16 +0200

    + Common Character Count

--- src/main/java/smoothSailing/CommonCharacterCount.java
+++ src/main/java/smoothSailing/CommonCharacterCount.java
@@ -1,24 +1,37 @@
 package smoothSailing;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class CommonCharacterCount {
     //Given two strings, find the number of common characters between them.     licza wspolnych znakow
 
-    int commonCharacterCount(String s1, String s2) {
+    static int commonCharacterCount(String s1, String s2) {
 
-        char [] char1 = s1.toCharArray();
-        char [] char2 = s2.toCharArray();
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        Arrays.fill(freq1, 0);
+        Arrays.fill(freq2, 0);
 
-        int [] characters1 = new int[26];
-        int [] characters2 = new int[26];
-            int commonCharacter =0;
 
-       for(char ch = 'a'; ch <= 'z'; ch++ ){
-           for(int i = 0; i  )
-       }
+        int count = 0;
 
-        Stream.of(s1).filter(s -> s.length() == commonCharacter).toArray(String[]::new);
+        // Update the frequencies of
+        // the characters of string s1
+        for (int i = 0; i < s1.length(); i++) {
+            freq1[s1.charAt(i) - 'a']++;
+        }
+        // Update the frequencies of
+        // the characters of string s2
+        for (int i = 0; i < s2.length(); i++) {
+            freq2[s2.charAt(i) - 'a']++;
+        }
+
+        // Find the count of valid pairs
+        for (int i = 0; i < 26; i++) {
+            count += (Math.min(freq1[i], freq2[i]));
+        }
+
+        return count;
     }
-
 }
